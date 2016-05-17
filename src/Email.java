3b1c9f4c0f129942f4ca5e4927663bbc6736e013
willriggins <williamriggins2013@gmail.com>
2016-05-17 11:58:10 -0400

    Add constructors and methods

---
+++ src/Email.java
@@ -0,0 +1,27 @@
+/**
+ * Created by will on 5/17/16.
+ */
+public class Email {
+    Person author;
+    String subject;
+    String body;
+    String destination;
+    int time;
+
+    public Email(Person author, String subject, String body, String destination, int time) {
+        this.author = author;
+        this.subject = subject;
+        this.body = body;
+        this.destination = destination;
+        this.time = time;
+    }
+
+    public Email(String subject, String body, String destination) {
+        this.author = null;
+        this.subject = subject;
+        this.body = body;
+        this.destination = destination;
+        this.time = 0;
+
+    }
+}
--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,51 @@
 /**
  * Created by will on 5/16/16.
  */
-public class Person {
+// this.name says it's a field inside of a class that we're in right now.
+//void indicates that it doesnt return anything
+public class Person { // public is an access modifier, ie anything in java can access this class
+    static int familySize = 10;
     String name;
     int age;
     boolean isAlive;
-    Person[] family = new Person[10];
+    Person[] family = new Person[familySize];
+
+    public Person(String name, int age, boolean isAlive) {
+        this.name = name;
+        this.age = age;
+        this.isAlive = isAlive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        if (isValidName(newName)) {
+            name = newName;
+        }
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+
+    public boolean getIsAlive() {
+        return isAlive;
+    }
+
+    public void setIsAlive(boolean newIsAlive) {
+        isAlive = newIsAlive;
+    }
+
+    public static boolean isValidName(String newName) {
+        return newName.contains(" ");
+    }
+
+
 }
