084de11a2a5e8afb2698478440c81869bb701755
willriggins <williamriggins2013@gmail.com>
2016-06-08 10:21:57 -0400

    multi solutions in exercise 4

--- src/Exercise04.java
+++ src/Exercise04.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -9,10 +11,25 @@ public class Exercise04 {
 
         HashMap<String, String> namesMap = new HashMap<>();
 
+        //solution 1: traditional for loop
+
         for (int i = 0; i < names.length; i+=2) {
             String firstName = names[i];
             String lastName = names[i+1];
             namesMap.put(lastName, firstName);
+        }
+        System.out.println(namesMap);
+
+        // solution 2:
+        namesMap = new HashMap<>();
+
+        ArrayList<String> namesArr = new ArrayList<>(Arrays.asList(names));
+        ArrayList<String> pair;
+
+        while (namesArr.size() > 0) {
+            pair = new ArrayList<String>(namesArr.subList(0, 2));
+            namesArr = new ArrayList<String>(namesArr.subList(2, namesArr.size()));
+            namesMap.put(pair.get(1), pair.get(0));
         }
 
         System.out.println(namesMap);
